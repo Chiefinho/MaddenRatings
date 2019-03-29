e1495d7d9381c58c144b74204628a82718149c6d
Oliver Haine <Oliver.Haine@coins-global.com>
2019-03-29 23:44:23 +0000

    Initial Commit

---
+++ MaddenRatingsTests/MaddenRatingsTests.swift
@@ -0,0 +1,34 @@
+//
+//  MaddenRatingsTests.swift
+//  MaddenRatingsTests
+//
+//  Created by Oliver Haine on 29/03/2019.
+//  Copyright © 2019 Chief's Tips. All rights reserved.
+//
+
+import XCTest
+@testable import MaddenRatings
+
+class MaddenRatingsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
